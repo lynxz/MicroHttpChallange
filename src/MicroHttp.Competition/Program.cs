83c7aab8441fe263dc8c965f4bd570deeb33a585
anders.stromberg <anders.stroemberg@gmail.com>
2026-03-03 22:59:20 +0100

    adding competition service

---
+++ src/MicroHttp.Competition/Program.cs
@@ -0,0 +1,63 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = $"{builder.Configuration["Keycloak:BaseUrl"]}/realms/{builder.Configuration["Keycloak:Realm"]}",
+
+        ValidateAudience = true,
+        ValidAudience = "account",
+
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = false,
+
+        IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+        {
+            // Retrieve the public keys from Keycloak
+            var keyUri = $"{parameters.ValidIssuer}/protocol/openid-connect/certs";;
+            using var httpClient = new HttpClient();
+            var response = httpClient.GetStringAsync(keyUri).Result;
+            var keys = new JsonWebKeySet(response);
+            return keys?.GetSigningKeys();;
+        }
+    };
+
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+});
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    
+    return OkResult();
+});
+
+app.Run();
+
